84761c5962c8061552dd7c7b4293ba7753916379
vch2128 <153256002+vch2128@users.noreply.github.com>
2024-06-19 23:15:27 +0800

    if eg 1

---
+++ if-1.py
@@ -0,0 +1,34 @@
+test_1 = {
+    "name" : "Chiikawa",
+    "color" : "white",
+    "height" : 30,
+    "weed" : 5
+}
+
+test_2 = {
+    "name" : "Hachiware",
+    "color" : "white and blue",
+    "height" : 30,
+    "weed" : 4
+}
+
+# 修改Q1, Q2, Q3的結果，分別代表三個問題的答案
+Q1 = True
+Q2 = True
+Q3 = True
+
+# 輸出答案（不需要修改這行以下的程式碼）
+if Q1:
+    print("他們的顏色一樣！")
+else:
+    print("他們的顏色不一樣～")
+
+if Q2:
+    print("他們的身高一樣！")
+else:
+    print("他們的身高不一樣～")
+
+if Q3:
+    print("吉伊持有比較高階的除草證 ʕ·ᴥ·ʔ")
+else:
+    print("小八貓持有比較高階的除草證 ヽ(=^・ω・^=)丿")
