93ca15b6c8e1bcf50475fbb84711fe2f7ca8266c
vitolin0416 <vitolin0416@gmail.com>
2024-06-25 00:20:25 +0800

    remove the additional layer of folder

---
+++ class-1/dict.py
@@ -0,0 +1,10 @@
+Usagi = {
+    "color" : "white",
+    "birth_year" : 2019,
+  	"birthday" : "1/22",
+  	"test" : "failed"
+}
+# TODO: 怎麼修改和刪除字典的值？
+Usagi["???"] = "???"
+??? Usagi["???"]
+print(Usagi)
