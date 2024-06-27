620e380ecdd67303f9a3804c748f8d7ad8bcafbb
johashhh <>
2024-06-27 21:22:52 +0800

    change to 1 base

--- class-3/function_4.py
+++ class-3/function_4.py
@@ -1,11 +1,12 @@
 def fibonacci(n):
-    if n > 1:
+    if n > 2:
         return fibonacci(n-1) + fibonacci(n-2)
-    elif n == 0:
-        return 0
+    elif n == 2:
+        return 2
     elif n == 1:
         return 1
+    
 result = []
-for i in range(10):
+for i in range(1, 11):
     result.append(fibonacci(i))  # 把前十項費氏數列放進列表
 print(result)
