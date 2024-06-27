bffb48bdfeac81d63f7eaee3f316c4f2448318c8
johashhh <>
2024-06-27 20:41:17 +0800

    fix bugs

--- class-3/function_4.py
+++ class-3/function_4.py
@@ -1,8 +1,10 @@
 def fibonacci(n):
     if n > 1:
         return fibonacci(n-1) + fibonacci(n-2)
-    # n==0 或是 n==1
-    return 1
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
 result = []
 for i in range(10):
     result.append(fibonacci(i))  # 把前十項費氏數列放進列表
---
+++ class-3/math_problem.py
@@ -0,0 +1,11 @@
+import math
+
+def my_func(a, b, c):
+    ans_1 = float(__1__)# 第一個根 
+    ans_2 = float(__2__)# 第二個根
+    
+    # 輸出兩個根
+    print(__3__)
+    print(__4__)
+
+my_func(1, 4, -7)
